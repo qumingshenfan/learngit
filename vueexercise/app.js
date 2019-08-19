1364c24c14d410d9794cae8da43d76bbcb935233
lingling <nialice@126.com>
2019-08-20 00:18:18 +0800

    change1

--- vueexercise/app.js
+++ vueexercise/app.js
@@ -1,11 +1,30 @@
 var list = [
-    {
+  /*  {
         title:"吃饭打豆豆"
     },
     {
         title:"吃饭打豆豆"
-    }
+    }*/
 ];
 new Vue({
-    
+    el:".main",
+    data:{
+        list:list
+    },
+    methods: {
+        addTodo(ev){//添加任务
+            //向list中添加一项任务
+            /*
+            {
+
+            }
+             */
+
+             //事件处理函数中的this指向的是，当前这个根实例
+            if(ev.keyCode === 13)
+            this.list.push({
+                title:ev.target.value
+            })
+        }
+    }
 })
